2d4e6c9f787c1264024548b96bebecfb5181822f
Zakariya Ahmed <zakariya.ahmed34@gmail.com>
2015-11-18 18:05:52 -0600

    checkBoundries function will be implemented in the updates so when a sprite goes off the screen it wraps around to the other side of our world Lines 99-104 handle the x-axis, 106-111 handle the y-axis
    Shit's magic yo.

--- 1-Empty project/js/game.js
+++ 1-Empty project/js/game.js
@@ -53,6 +53,7 @@ gameState.prototype = {
 
     update: function () {
         this.checkPlayerInput();
+        this.checkBoundries(this.shipSprite);
     },
 
     initGraphics: function(){
@@ -91,6 +92,22 @@ gameState.prototype = {
         }
         else{
             this.shipSprite.body.acceleration.set(0);
+        }
+    },
+
+    checkBoundries: function(sprite){
+        if (sprite.x < 0){
+            sprite.x = game.width;
+        }
+        else if(sprite.x > game.width){
+            sprite.x = 0;
+        }
+
+        if(sprite.y < 0){
+            sprite.y = game.height;
+        }
+        else if(sprite.y > game.height){
+            sprite.y = 0;
         }
     },
 };
