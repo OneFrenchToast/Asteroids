1d42771f4fcb184510d8b6e9ca380da802486634
Zakariya Ahmed <zakariya.ahmed34@gmail.com>
2015-11-26 02:10:54 -0600

    Added background music to our end game page along with the necessary assets

--- js/end.js
+++ js/end.js
@@ -2,10 +2,17 @@ var potato = {
 
     preload: function() {
         game.load.image('endScreen', 'assets/EndingPage.png');
+
+        game.load.audio('champion', ['assets/Champions.ogg', 'assets/Champions.m4a']);
     },
 
     create: function() {
         this.add.button(0, 0, 'endScreen', this.resetGame, this);
+
+        this.music = this.game.add.audio('champion');
+        this.music.volume = 0.5;
+        this.music.loop = true;
+        this.music.play();
     },
 
     resetGame: function() {
